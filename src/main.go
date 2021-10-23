9a5683d1d69640cd325bcdb645fcf02cb5df274d
rune <rune@ruune.de>
2021-10-23 18:54:22 +0200

    added limiting login during time of day period

--- src/main.go
+++ src/main.go
@@ -21,6 +21,10 @@ type timesStruct struct {
 		Tue int `json:"Tue"`
 		Wed int `json:"Wed"`
 	} `json:"maxTimes"`
+	DayTimes struct {
+		NotAfter  string `json:"notAfter"`
+		NotBefore string `json:"notBefore"`
+	} `json:"dayTimes"`
 	TimeLeft struct {
 		Left int    `json:"left"`
 		Date string `json:"date"`
@@ -48,12 +52,28 @@ func (times timesStruct) getLimit(now time.Time) int {
 	return 4
 }
 
+func logout() {
+	err := beeep.Notify("TimeLimiter", "Logout in 15 seconds", "")
+	if err != nil {
+		panic(err)
+	}
+	time.Sleep(15 * time.Second)
+	if err := exec.Command("cmd", "/C", "shutdown", "/l").Run(); err != nil {
+		fmt.Println("Failed to initiate logout:", err)
+	}
+}
+
+func getBeforeAfterTime(now time.Time, ba string) time.Time {
+	a, _ := time.Parse("1504", ba)
+	return time.Date(now.Year(), now.Month(), now.Day(), a.Hour(), a.Minute(), a.Second(), a.Nanosecond(), now.Location())
+}
+
 func main() {
 	//get json data
 	data, err := os.ReadFile("./time.json")
 	if err != nil {
 		//if file doesnt exist
-		data = []byte(`{"maxTimes":{"Fri":120,"Mon":120,"Sat":120,"Sun":120,"Thu":120,"Tue":120,"Wed":120},"timeLeft":{"left":0,"date":"` + time.Now().Format("01-02-2006") + `"}}`)
+		data = []byte(`{"maxTimes":{"Fri":120,"Mon":120,"Sat":120,"Sun":120,"Thu":120,"Tue":120,"Wed":120},"dayTimes": {"notAfter": "0100","notBefore": "0800"},"timeLeft":{"left":0,"date":"` + time.Now().Format("01-02-2006") + `"}}`)
 		err = os.WriteFile("./time.json", data, 0644)
 		if err != nil {
 			fmt.Println(err)
@@ -65,18 +85,34 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(times.TimeLeft.Left)
 
 	startTime := time.Now()
-	now := time.Now()
-	var timeLeft int
+
+	//check if notAfter and notBefore are on the same day, add one day if not
+	beforeAddDay := 0
+	notAfter := getBeforeAfterTime(startTime, times.DayTimes.NotAfter)
+	notBefore := getBeforeAfterTime(startTime, times.DayTimes.NotBefore)
+	if notAfter.After(notBefore) {
+		beforeAddDay = 1
+	}
 
 	//time for logout
 	endTime := startTime.Add(time.Minute * time.Duration(times.TimeLeft.Left))
 
 	for range time.Tick(time.Minute * 1) {
 		//current time
-		now = time.Now()
+		now := time.Now()
+
+		//time of day
+		notAfter := getBeforeAfterTime(now, times.DayTimes.NotAfter)
+		notBefore := getBeforeAfterTime(now.AddDate(0, 0, beforeAddDay), times.DayTimes.NotBefore)
+
+		//logout when not during allowed time of day
+		if now.After(notAfter) && now.Before(notBefore) {
+			logout()
+		}
+
+		//limit
 		//if next day started, start timer from limit
 		if times.TimeLeft.Date != now.Format("01-02-2006") {
 			startTime = time.Now()
@@ -86,7 +122,7 @@ func main() {
 		}
 
 		//time left
-		timeLeft = int(endTime.Sub(now).Minutes())
+		timeLeft := int(endTime.Sub(now).Minutes())
 		times.TimeLeft.Left = timeLeft
 
 		//notify when 10 and 2 minutes left
@@ -109,9 +145,7 @@ func main() {
 
 		//logout after time runs out
 		if now.After(endTime) {
-			if err := exec.Command("cmd", "/C", "shutdown", "/l").Run(); err != nil {
-				fmt.Println("Failed to initiate logout:", err)
-			}
+			logout()
 		}
 	}
 }
